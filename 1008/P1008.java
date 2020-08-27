9b896a1c60dc3cafdd6526cdc79f0cf3a9e455d1
v1r4m <choyoung7789@gmail.com>
2020-08-27 11:59:51 +0900

    P1008.java

---
+++ 1008/P1008.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class P1008{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextInt();
+        double b = sc.nextInt();
+        sc.close();
+        double c;
+        c = a/b;
+        System.out.print(c);
+
+    }
+}
