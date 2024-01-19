3acf050bd35855375dca7827d7cf170386ca0da9
v1r4m <choyoung7789@gmail.com>
2024-01-19 21:09:24 +0900

    adjust input

--- 1932/1932.py
+++ 1932/1932.py
@@ -1,7 +1,10 @@
-a = int(input())
+from sys import stdin as s
+s = open("1932/input.txt","rt")
+
+a = int(s.readline().strip())
 arr = []
 for i in range(a):
-    temp = list(map(int,input().split()))
+    temp = list(map(int,s.readline().split()))
     arr.append(temp)
 dp = [[0 for i in range(a)] for i in range(a)]
 dp[0][0] = arr[0][0]
