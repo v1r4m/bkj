2fe87b5a1dc89699a2adb3bd5592aa908405c3d6
v1r4m <choyoung7789@gmail.com>
2024-11-17 17:34:06 +0900

    20955ㅠㅠㅠㅠ

--- 20955/20955.py
+++ 20955/20955.py
@@ -2,40 +2,48 @@ from sys import stdin as s
 
 s = open("20955/input.txt", "rt")
 n, m = map(int, s.readline().split())
-snaps = [[False for _ in range(n+1)] for _ in range(n+1)]
-a,b = map(int, s.readline().split())
-snaps[a][b]=True
-snaps[b][a]=True
-root = a
-root_connected = [False for _ in range(n+1)]
-root_connected[root]=True
-for i in range(2,m+1):
-    a,b = map(int, s.readline().split())
-    snaps[a][b]=True
-    snaps[b][a]=True
-traverse = []
-for i in range(n+1):
-    if snaps[root][i]:
-        traverse.append((i,root))
-work = 0
-flag = 1
-while flag:
-    while traverse:
-        next_traverse, before_traverse = traverse.pop()
-        if root_connected[next_traverse]:#visited
-            work=work+1
-        else:#good
-            for i in range(n+1):
-                if snaps[next_traverse][i] and i != before_traverse:
-                    traverse.append((i,next_traverse))
-                root_connected[next_traverse]=True
-    for i in range(1,n+1):
-        if root_connected[i]==False:
-            work = work+1
-            #attach root and i
-            root_connected[i]=True
-            traverse.append((i,root))
-            break
-        else:
-            flag=0
-print(work)
+
+# 인접 리스트 초기화
+snaps = [[] for _ in range(n + 1)]
+
+a, b = map(int, s.readline().split())
+snaps[a].append(b)
+snaps[b].append(a)
+
+for _ in range(2, m + 1):
+    a, b = map(int, s.readline().split())
+    snaps[a].append(b)
+    snaps[b].append(a)
+
+visited = [False] * (n + 1)
+
+def dfs(node):
+    stack = [node]
+    visited[node] = True
+    node_count = 0  # 해당 컴포넌트에 포함된 노드 수
+    edge_count = 0  # 해당 컴포넌트에 포함된 간선 수
+
+    while stack:
+        current = stack.pop()
+        node_count += 1
+        for neighbor in snaps[current]:
+            edge_count += 1
+            if not visited[neighbor]:
+                visited[neighbor] = True
+                stack.append(neighbor)
+    
+    edge_count //= 2
+    return node_count, edge_count
+
+extra_edges = 0  
+connected_components = 0
+
+for i in range(1, n + 1):
+    if not visited[i]:
+        node_count, edge_count = dfs(i)
+        extra_edges += edge_count - (node_count - 1)
+        connected_components += 1
+
+extra_edges += connected_components - 1
+
+print(extra_edges)
